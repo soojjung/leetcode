c5b0dc82805b1509a1c3ef1fd8b6fec4ea38e692
soojjung <89066676+soojjung@users.noreply.github.com>
2024-07-06 01:44:08 +0900

    Time: 48 ms (82.7%), Space: 48.3 MB (88.96%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let count = 0;
+    while (num > 0) {
+        if (num % 2 === 0) {
+            num = num / 2
+            count += 1
+        }
+        if (num % 2 === 1) {
+            num = num - 1
+            count += 1
+        }
+    }
+    return count
+};
