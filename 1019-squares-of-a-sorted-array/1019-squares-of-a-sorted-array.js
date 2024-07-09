40f467a84c2902f391791e6f9fd4f8b314cdd325
soojjung <89066676+soojjung@users.noreply.github.com>
2024-07-10 00:15:08 +0900

    Time: 79 ms (78.02%), Space: 57.3 MB (5.4%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.js
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.js
@@ -2,6 +2,19 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortedSquares = function(nums) {
-    return nums.map(i => i**2).sort((a,b) => a-b)
+var sortedSquares = function (nums) {
+  const result = new Array(nums.length).fill(0);
+  let start = 0;
+  let end = nums.length - 1;
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (Math.abs(nums[start]) > Math.abs(nums[end])) {
+      result[i] = nums[start] ** 2;
+      start++;
+    } else {
+      result[i] = nums[end] ** 2;
+      end--;
+    }
+  }
+  return result;
 };
