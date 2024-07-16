b18990e4428c1107cba04d904f97b4ceb489cf52
soojjung <89066676+soojjung@users.noreply.github.com>
2024-07-16 23:30:43 +0900

    Time: 57 ms (29.41%), Space: 48.7 MB (55.5%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.js
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var minStartValue = function(nums) {
-    const startValue = 0;
+    const initialValue = 0;
     let minSum = 100;
 
     nums.reduce((acc, cur) => {
         minSum = Math.min(minSum, acc + cur)
         return acc + cur
-    }, startValue)
+    }, initialValue)
   
     return minSum < 1 ? 1 - minSum : 1
 };
