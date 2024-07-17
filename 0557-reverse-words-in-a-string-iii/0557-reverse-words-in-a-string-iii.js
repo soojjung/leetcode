6129fac640e24633bc5e2f8e01404358c1f7bc32
soojjung <89066676+soojjung@users.noreply.github.com>
2024-07-18 00:18:59 +0900

    Time: 55 ms (94.29%), Space: 54.8 MB (90.01%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -3,11 +3,5 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    const arr = s.split(' ');
-    let newString = ''
-
-    for (let i =0; i<arr.length; i++) {
-        newString += arr[i].split('').reverse().join('') + ' '
-    }
-    return newString.trim()
+    return s.split(' ').map((word) => word.split('').reverse().join('')).join(' ')
 };
