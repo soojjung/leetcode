a52e290cb4e925f45fd472e18bdee7b67f23d135
soojjung <89066676+soojjung@users.noreply.github.com>
2024-07-25 18:06:38 +0900

    Time: 51 ms (97.69%), Space: 51.7 MB (71.54%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.js
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.js
@@ -3,11 +3,11 @@
  * @return {number[]}
  */
 var intersection = function(nums) {
-    const interArr = nums.reduce((acc, cur) => {
+    const intersection = nums.reduce((acc, cur) => {
         const set1 = new Set(acc);
         const set2 = new Set(cur);
         return [...set1].filter(item => set2.has(item));
     })
     
-    return interArr.sort((a, b) => a - b);
+    return intersection.sort((a, b) => a - b);
 };
