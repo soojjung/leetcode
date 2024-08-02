4eaff281338a701e62e16cf0931f06cedb38a9d7
soojjung <89066676+soojjung@users.noreply.github.com>
2024-08-02 20:36:53 +0900

    Time: 95 ms (21.63%), Space: 66.5 MB (8.88%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const hashMap = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (hashMap.has(nums[i])) {
+            return true;
+        }
+        hashMap.set(nums[i], 1);
+    }
+    return false;
+};
