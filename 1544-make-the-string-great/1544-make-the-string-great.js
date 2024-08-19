c965e9dcb76b6ccfcdef3dd66413bc4d95d377cb
soojjung <89066676+soojjung@users.noreply.github.com>
2024-08-20 01:46:26 +0900

    Time: 66 ms (27.11%), Space: 50.4 MB (85.19%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeGood = function(s) {
+    const stack = [];
+    for (const char of s) {
+        if (stack.length > 0 && Math.abs(char.charCodeAt() - stack[stack.length - 1].charCodeAt()) === 32) {
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.join('');
+};
