3462e5922eba93fc9b485005195d0e8501b20e12
Soojin Jung <89066676+soojjung@users.noreply.github.com>
2024-09-10 21:43:54 +0900

    Time: 54 ms (80.02%), Space: 51.9 MB (14.59%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    const ans = [];
+
+    const dfs = (node, depth) => {
+        if (!node) {
+            return;
+        }
+
+        if (depth >= ans.length) {
+            ans.push(node.val);
+        }
+
+        dfs(node.right, depth + 1);
+        dfs(node.left, depth + 1);
+    }
+
+    dfs(root, 0);
+
+    return ans;
+};
